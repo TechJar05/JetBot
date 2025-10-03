deff17ee8a7ea558592dda7a2666affd7b443c0d
sairaj <sairajt70@gmail.com>
2025-10-03 11:18:31 +0530

    increased access token time

--- JetBot/settings.py
+++ JetBot/settings.py
@@ -161,7 +161,7 @@ DATABASES = {
 
 # Simple JWT settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
